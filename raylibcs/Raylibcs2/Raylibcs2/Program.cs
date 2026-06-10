3240e6d63097a4773b087a961e29b84bc7c9b325
m@3 <nonenone1124@gmail.com>
2026-06-10 19:46:44 +0900

    Add files via upload

--- raylibcs/Raylibcs2/Raylibcs2/Program.cs
+++ raylibcs/Raylibcs2/Raylibcs2/Program.cs
@@ -368,7 +368,8 @@ namespace RaylibSideScrollerShooter
                 //                enemies.Add(new Enemy(ScreenWidth + 50,
                 //                  Raylib.GetRandomValue(50, ScreenHeight - 80), 50, 40));
                 enemies.Add(new Enemy(GameConfig.ScreenWidth / GameConfig.X_SCALE + 0f,
-                    Raylib.GetRandomValue(32, GameConfig.ScreenHeight / GameConfig.Y_SCALE - 32 - 32), 0f, 5f / COUNT1S, type, 0f, 0, hp, false));
+                    Raylib.GetRandomValue(32, GameConfig.ScreenHeight / GameConfig.Y_SCALE - 32 - 32), 0f, 5f / COUNT1S, type, 0f,
+                        Raylib.GetRandomValue(-30*2, GameConfig.ScreenHeight / GameConfig.Y_SCALE - 40 * 2), hp, false));
                     enemySpawnTimer = 0f; // (float)Raylib.GetRandomValue(60, 120) / 100f;
             }
 
